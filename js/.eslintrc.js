d29307349f9d25e5bcb7a683a6e03fd00b56092e
akrasnov96 <akrasnov96@yandex-team.ru>
2018-11-18 13:51:37 +0300

    update eslint settings update package info sml fix for .editorconfig

--- js/.eslintrc.js
+++ js/.eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-    "extends": "eslint:recommended",
-    "env": {
-        "jest": true
-    }
+  "extends": "eslint:recommended",
+  "env": {
+    "jest": true,
+    "es6": true,
+    "node": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 2017
+  },
 };
