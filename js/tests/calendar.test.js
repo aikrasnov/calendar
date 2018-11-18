50a1205fbb079e6f402c078ec79dab7e4113a9cb
akrasnov96 <akrasnov96@yandex-team.ru>
2018-11-18 14:04:11 +0300

    add jest and first test

---
+++ js/tests/calendar.test.js
@@ -0,0 +1,11 @@
+const {Calendar} = require('../calendar');
+const assert = require('assert');
+
+const calendar = new Calendar();
+
+describe('main', () => {
+
+  it('should have year, month, day', async () => {
+    assert(typeof calendar.getYear() === 'number');
+  });
+});
